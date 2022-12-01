77ddffd8634b8c34f229479e541a8d0886cce394
ben11997 <abrahambenson90@gmail.com>
2022-12-01 08:22:40 -0800

    No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- List all shows contained in 'hbtn_0d_tvshows' without a genre linked
 -- Each record should display tv_shows.title, tv_show_genres.genre_id
--- Results must be sorted ascending order by tv_shows.title and tv_show_genres.genres_id
+-- Results must be sorted ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
