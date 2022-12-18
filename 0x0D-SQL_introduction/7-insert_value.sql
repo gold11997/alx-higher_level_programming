378837099433df13753c31af56dc7c5bb68e0a10
ben11997 <abrahambenson90@gmail.com>
2022-12-18 14:15:19 -0800

    fixing code

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
--- inserts a new row in table first_table
---in database hbtn_0c_0
-INSERT INTO first_table(id, name) VALUES (89, 'Best School');
+-- Inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server
+INSERT INTO first_table VALUES(89, 'Best School')
