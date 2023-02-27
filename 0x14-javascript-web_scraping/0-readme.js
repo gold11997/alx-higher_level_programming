0274f8c378faf761a945bcca9d52e8de3d71da98
ben11997 <abrahambenson90@gmail.com>
2023-02-27 23:43:15 +0100

    0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const path = require('path');
-
-const filePath = process.argv[2];
-
-fs.readFile(path.resolve(filePath), 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
